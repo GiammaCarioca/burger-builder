aa8e6a3b195efa3216c0198deeb54055150ff349
Thiago Giammattey <giammattey@gmail.com>
2018-09-06 14:35:49 -0300

    added the Layout component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import './styles/global';
 
-const App = () => <h1>Burger Builder</h1>;
+import Layout from './components/Layout/Layout';
+
+const App = () => (
+  <div>
+    <Layout>
+      <p>Test</p>
+    </Layout>
+  </div>
+);
 
 export default App;
